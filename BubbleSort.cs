44b3250ded56a1c01b4af3542857f3cc0cd057d2
Noe De La Mora <Noe.DeLaMora@univarusa.com>
2016-06-07 14:58:22 -0500

    new script, haven't implemented a sort yet.

---
+++ BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace doodles {
+    class BubbleSort
+    {
+        public int[] randomsArray { get; set; }
+
+        public BubbleSort()
+        {
+            Random randomnumber = new Random();
+            randomsArray = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                randomsArray[i] = randomnumber.Next(0, 300);
+            }
+        }
+    }
+
+    public class MainClass {
+        static void Main() {
+            BubbleSort bubble = new BubbleSort();
+
+            //Console.WriteLine(bubble.randomsArray[0]);
+
+            foreach (int bubba in bubble.randomsArray)
+            {
+                Console.WriteLine(bubba.ToString());
+            }
+        }
+    }
+}
